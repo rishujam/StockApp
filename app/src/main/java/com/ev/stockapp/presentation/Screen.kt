800c3574f98f2a53af66156bdbe7a587564caa7c
rishu <rishuparashar7@gmail.com>
2022-10-15 00:35:18 +0530

    test

--- app/src/main/java/com/ev/stockapp/presentation/Screen.kt
+++ app/src/main/java/com/ev/stockapp/presentation/Screen.kt
@@ -3,4 +3,6 @@ package com.ev.stockapp.presentation
 sealed class Screen(val route:String) {
     object CompanyListings:Screen("company_listings_screen")
     object CompanyInfo: Screen("company_info_screen")
+
+    //Implement
 }
